b370126a026e8f198dfa477b2b3b7c7a6f75d03d
kenfutamata <104425231+kenfutamata@users.noreply.github.com>
2023-11-15 10:16:29 +0800

    Add files via upload

---
+++ Activity no.4/CreateJFrameWithComponents.java
@@ -0,0 +1,16 @@
+import javax.swing.*; 
+
+public class CreateJFrameWithComponents{
+
+public static void main(String[]args){
+
+JFrameWithComponents aFrame = new JFrameWithComponents(); 
+
+final int width = 350; 
+final int height = 100; 
+aFrame.setSize(width, height); 
+aFrame.setVisible(true); 
+   
+   }
+
+}
